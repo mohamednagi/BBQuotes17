f8578ec5e4bc8d672db0b1054dd5fe688cdf3837
mnagi <mnagi@youxel.com>
2025-01-06 20:42:47 +0200

    Initial Commit

---
+++ BBQuotes17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBQuotes17
+//
+//  Created by Mohamed Nagi on 06/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
